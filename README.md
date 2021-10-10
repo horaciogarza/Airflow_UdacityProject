1b8233b50e9e3227065ad836cbd8300ccd8d4117
Horacio Garza <hgx95@hotmail.com>
2021-10-10 15:02:40 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Data Pipeline project (Apache Airflow)
+
+This project was fully created by Horacio Garza, this project uses Apache Airflow 
+
+
+
+## [](https://github.com/horaciogarza/Airflow_UdacityProject#Files)Files
+
+Files:
+
+-   dags (folder): Folder containing the udac_example_dag provided by udacity
+-   create_tables.sql: File containing all the queries that are going to be exewcuted in Redshift
+-   plugins (folder): Folder containing helpers/operators
+
+
+## [](https://github.com/horaciogarza/Airflow_UdacityProject#requisites)Requisites
+
+-   Python
+-   configparser Python library
+-   AWS Account (With Redshift)
+- 	Apache Airflow
---
+++ plugins/operators/load_dimension.py
@@ -0,0 +1,46 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 sql_query="",
+                 skip_truncate=False,
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query = sql_query
+        self.skip_truncate = skip_truncate
+
+    #Executing function to execute the SQL queries defined here.
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        sql_code = ""
+
+        if self.skip_truncate:
+            sql_code = f"""
+                    BEGIN;
+                    
+                    INSERT INTO {self.table}
+                    
+                    {self.sql_query};"""
+        else:
+            sql_code = f"""
+                    BEGIN;
+                    TRUNCATE TABLE {self.table}; 
+                    
+                    INSERT INTO {self.table}
+                    
+                    {self.sql_query};"""
+
+
+        redshift.run(sql_code)
